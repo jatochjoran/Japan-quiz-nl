6e57a938442c4b0431a1d7f0efb95251376343c4
jatochjoran <90198387+jatochjoran@users.noreply.github.com>
2024-03-14 09:33:43 +0100

    loading screen toegevoegd aan home screen

---
+++ index/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Simulate loading for 5 seconds
+    setTimeout(function() {
+        // Hide the loading screen with fade out animation
+        $(".circle").fadeOut("slow", function() {
+            // Show the content after the loading screen is hidden
+            $("#content2").fadeIn("slow");
+        });
+    }, 5000);
+});
