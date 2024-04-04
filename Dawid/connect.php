a2e2f179a86e663a743d19c2f72520a8049a5108
NOLLXX <156187627+NOLLXX@users.noreply.github.com>
2024-04-04 11:08:17 +0200

    php en AJAX
    aafasdfasdgasdf

---
+++ Dawid/connect.php
@@ -0,0 +1,22 @@
+<?php
+$host = "localhost";
+$username = "101177";
+$password = "J@p@n101177!";
+$database = "docent_quiz";
+
+
+$con = mysqli_connect(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+
+$playerName = $_POST['playerName'];
+$score = $_POST['score'];
+$quizType = $_POST['quizType'];
+
+// Voer een SQL query uit om de data in te voegen
+$sql = "INSERT INTO jeTabel (Naam, Score, Quiz_Type) VALUES (" . $playerName . ", " . $score . ", " . $quizType . ")";
+?>
